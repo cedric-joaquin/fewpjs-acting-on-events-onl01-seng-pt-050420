8480ce611431979cdbbde63e514fa271fea8f75e
cedric-joaquin <joaquin.cedric@gmail.com>
2020-11-03 18:35:07 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+let dodger = document.getElementById("dodger")
+
+function moveDodgerLeft() {
+    let leftNumber = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumber, 10)
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumber = dodger.style.left.replace("px", "")
+    let right = parseInt(leftNumber, 10) + 40
+
+    if (right < 360) {
+        dodger.style.left = `${parseInt(leftNumber,10) + 1}px`
+    }
+}
